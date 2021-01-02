26167fa886dc03909d2b1471328465bd15ddb742
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-01-02 18:30:55 +0100

    Added Station Info

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,8 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+
+    <link rel="stylesheet" href="../css/dashboard_stylesheet.css">
 </head>
 <body>
-
+    <div class="station-info" onload="updateTime()">
+        <h1 class="station-location">VILLNÖSS</h1>
+        <p class="station-location-further">Bressanone, Italy</p>
+        <a class="station-coordinates-time" href="https://www.google.com/maps/search/46.642,+11.675">46.64, 11.67</a>
+    </div>
 </body>
 </html>
