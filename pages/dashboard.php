aec7bc9736a04cf4847158b7aada02d46527e524
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-03-16 16:13:30 +0100

    Added Greeting

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -76,7 +76,32 @@ if (isset($_SESSION['measUnit'])) {
 </head>
 <body>
 
-<div></div>
+<div class="greeting">
+    <h1 class="heading-greeting" id="heading-greeting"></h1>
+    <script>
+        let name = '<?php echo $_SESSION['firstName'] ?>'
+        document.getElementById("heading-greeting").innerHTML = "Hallo " + name;
+    </script>
+    <h3 class="prompt-greeting">Sieh dir die heutigen Messungen an!</h3>
+    <img onclick="scrollDown()" src="../sources/Scroll.gif" style="height: 100px; width: 100px; margin-left: 650px" alt="scroll gif">
+    <script type="text/javascript">
+        var scrolledUnderPoint = false;
+
+        window.addEventListener("scroll", function (){
+            if(scrolledUnderPoint === false && window.pageYOffset >= 240){
+                scrolledUnderPoint = true;
+            }
+
+            if(scrolledUnderPoint && window.pageYOffset <= 240){
+                window.scrollTo(0, 240);
+            }
+        });
+
+        function scrollDown(){
+            window.scrollBy(0, 239);
+        }
+    </script>
+</div>
 
 <div class="station-info">
     <h1 class="station-location">VILLNÖSS</h1>
