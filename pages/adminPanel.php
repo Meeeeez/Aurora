52065fe834505cd57629145bf849c79c5e0aadcd
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-03-17 10:31:12 +0100

    Added sql functionalities

--- pages/adminPanel.php
+++ pages/adminPanel.php
@@ -1,3 +1,43 @@
+<?php
+    $servername = "weatherwebapp-db-new.cikkod1lareu.us-east-1.rds.amazonaws.com";
+    $username = "stationAdministrator";
+    $password = "StatioAdministrator123!";
+    $dbname = "WeatherWebApp";
+
+    if(isset($_GET['submit'])){
+        if($_GET['submit'] == 'delUser'){
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "DELETE FROM TUser WHERE foreignRoleID = 2";
+
+            $conn->close();
+        }else if($_GET['submit'] == 'delMeas'){
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "DELETE FROM TMeasurement";
+
+            $conn->close();
+        }else if($_GET['submit'] == 'delProt'){
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "DELETE FROM TProtocol";
+
+            $conn->close();
+        }else if($_GET['submit'] == 'seeProt'){
+            //look at protocol
+        }
+    }
+?>
+
 <style>
     .adminPanel {
         background-color: white;
@@ -18,46 +58,31 @@
         let adminName = '<?php echo $_SESSION['firstName'] ?>'
         adminGreeting.innerHTML = "Hello " + adminName + "! What do you want to do?";
     </script>
-    <ul>
-        <li>
-            <button id="button1" onmouseover="mouseEnter()" onclick="deleteUsers()" class="adminButton">Delete all Users</button>
-        </li>
-        <li>
-            <button id="button2" onmouseover="mouseEnter()" onclick="clearProtocol()" class="adminButton">Clear Protocol</button>
-        </li>
-        <li>
-            <button id="button3" onmouseover="mouseEnter()" onclick="deleteMeasurements()" class="adminButton">Delete all Measurements</button>
-        </li>
-    </ul>
+    <form action="" method="GET">
+        <ul>
+            <li>
+                <button name="submit" id="button4" onmouseover="mouseEnter()" value="seeProt" class="adminButton">Check Protocol</button>
+            </li>
+            <li>
+                <button name="submit" id="button1" onmouseover="mouseEnter()" value="delUser" class="adminButton">Delete all Users</button>
+            </li>
+            <li>
+                <button name="submit" id="button2" onmouseover="mouseEnter()" value="delProt" class="adminButton">Clear Protocol</button>
+            </li>
+            <li>
+                <button name="submit" id="button3" onmouseover="mouseEnter()" value="delMeas" class="adminButton">Delete all Measurements</button>
+            </li>
+        </ul>
+    </form>
+
 
     <script type="text/javascript">
         function mouseEnter(){
             document.getElementById("button1").style.cursor = "pointer";
             document.getElementById("button2").style.cursor = "pointer";
             document.getElementById("button3").style.cursor = "pointer";
+            document.getElementById("button4").style.cursor = "pointer";
         }
     </script>
 </div>
-<img id="scrollGif2" onmouseover="document.getElementById('scrollGif2').style.cursor = 'pointer'" onclick="if(window.pageYOffset === 0) window.scrollBy(0, 320)" src="../sources/Scroll.gif" style="height: 100px; width: 100px; margin-left: 650px" alt="scroll gif">
-<script type="text/javascript">
-
-    function deleteUser(){
-        <?php
-            try {
-                $servername = "weatherwebapp-db-new.cikkod1lareu.us-east-1.rds.amazonaws.com";
-                $username = "stationAdministrator";
-                $password = "StatioAdministrator123!";
-                $dbname = "WeatherWebApp";
-
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt = $conn->prepare("DELETE FROM TUser WHERE foreignRoleID = 2;");
-                $stmt->execute();
-            } catch(PDOException $e) {
-                echo "Error: " . $e->getMessage();
-            }
-            $conn = null;
-        ?>
-    }
-
-</script>
+<img id="scrollGif2" onmouseover="document.getElementById('scrollGif2').style.cursor = 'pointer';" onclick="if(window.pageYOffset === 0) window.scrollBy(0, 350);" src="../sources/Scroll.gif" style="height: 100px; width: 100px; margin-left: 650px" alt="scroll gif">
