d79e13c1d920d3d81cbe72451ae042549f286f86
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-03-18 08:18:21 +0100

    Destroyed session after receiving an error

--- pages/error.php
+++ pages/error.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    $_SESSION = array();
+    if (ini_get("session.use_cookies")){
+        $par = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000, $par["path"], $par["domain"], $par["secure"], $par["httponly"]);
+    }
+    session_destroy();
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
